5d6bc300cc2138045457f9bae511e911242759a0
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2024-09-02 19:15:27 +0200

    Added the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "135f02bffcta720ff4c241394b466o99";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Voorburg");
